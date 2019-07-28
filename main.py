a9b64c9d5d5cce69af5aac0650033b484e8967b3
Vivian Ta <vivian.ta@daimler.com>
2019-07-28 00:27:45 -0700

    initial commit

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 import sys
-import json
-
 import requests
-
-# Use Like python githubber.py JASchilz
-# (or another user name)
 
 if __name__ == "__main__":
     username = sys.argv[1]
+    response = requests.get(f"https://api.github.com/users/{username}/events")
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
-
-    print("COMPLETE THE TODOs")
+    print(f"The first github event from user {username} was at {response.json()[0]['created_at']}.")
     
-
-
